c3ffb302aa25f72f22bd0891ffafd0f757fd3af5
Ralph Holzmann <ralphholzmann@gmail.com>
2013-01-07 13:25:14 -0600

    Adding once handler.

--- Binder.js
+++ Binder.js
@@ -4,14 +4,22 @@ module.exports = (function() {
     this.bindings = [];
   }
 
-  Binder.prototype.bind = function(emitter, event, fn, context) {
+  Binder.prototype._bind = function(method, emitter, event, fn, context) {
     var bound = fn.bind(context);
     this.bindings.push({
       emitter: emitter,
       event: event,
       fn: bound
     });
-    emitter.on(event, bound);
+    emitter[method](event, bound);
+  };
+
+  Binder.prototype.once = function(emitter, event, fn, context) {
+    this._bind("once", emitter, event, fn, context);
+  };
+
+  Binder.prototype.bind = function(emitter, event, fn, context) {
+    this._bind("on", emitter, event, fn, context);
   };
 
   Binder.prototype.destroy = function() {
